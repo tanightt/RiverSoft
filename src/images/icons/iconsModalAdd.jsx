eba98b1df5daf9f7ef436dad2ff2ce4ff769eb07
Vladyslav <vladyslavromanenkoo@gmail.com>
2023-07-20 15:51:30 +0300

    styles to modalAdd

---
+++ src/images/icons/iconsModalAdd.jsx
@@ -0,0 +1,130 @@
+export const svgClose = (
+  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none">
+    <path stroke="#FBFBFB" d="m1 1 16 16M1 17 17 1" />
+  </svg>
+);
+
+export const svgCalendar = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+  >
+    <g clip-path="url(#clip0_60_133)">
+      <path
+        d="M9 11H7V13H9V11ZM13 11H11V13H13V11ZM17 11H15V13H17V11ZM19 4H18V2H16V4H8V2H6V4H5C3.89 4 3.01 4.9 3.01 6L3 20C3 21.1 3.89 22 5 22H19C20.1 22 21 21.1 21 20V6C21 4.9 20.1 4 19 4ZM19 20H5V9H19V20Z"
+        fill="#734AEF"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_60_133">
+        <rect width="24" height="24" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+export const svgPlus = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="74"
+    height="74"
+    viewBox="0 0 74 74"
+    fill="none"
+  >
+    <g filter="url(#filter0_d_60_139)">
+      <circle cx="37" cy="31" r="22" fill="#FFB627" />
+    </g>
+    <path d="M37 21V41" stroke="#FBFBFB" stroke-width="2" />
+    <path d="M27 31L47 31" stroke="#FBFBFB" stroke-width="2" />
+    <defs>
+      <filter
+        id="filter0_d_60_139"
+        x="0"
+        y="0"
+        width="74"
+        height="74"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="6" />
+        <feGaussianBlur stdDeviation="7.5" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 1 0 0 0 0 0.780392 0 0 0 0 0.152941 0 0 0 0.5 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="BackgroundImageFix"
+          result="effect1_dropShadow_60_139"
+        />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="effect1_dropShadow_60_139"
+          result="shape"
+        />
+      </filter>
+    </defs>
+  </svg>
+);
+
+export const svgMinus = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="74"
+    height="74"
+    viewBox="0 0 74 74"
+    fill="none"
+  >
+    <g filter="url(#filter0_d_34_5533)">
+      <circle cx="37" cy="31" r="22" fill="#FF868D" />
+    </g>
+    <path d="M27 31L47 31" stroke="white" stroke-width="2" />
+    <defs>
+      <filter
+        id="filter0_d_34_5533"
+        x="0"
+        y="0"
+        width="74"
+        height="74"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="6" />
+        <feGaussianBlur stdDeviation="7.5" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 1 0 0 0 0 0.52549 0 0 0 0 0.552941 0 0 0 0.5 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="BackgroundImageFix"
+          result="effect1_dropShadow_34_5533"
+        />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="effect1_dropShadow_34_5533"
+          result="shape"
+        />
+      </filter>
+    </defs>
+  </svg>
+);
