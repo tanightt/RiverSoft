b954a2e93fce582654400a146276764003057772
endorf8n <128092594+endorf8n@users.noreply.github.com>
2023-07-21 23:05:58 +0300

    Update Balance.jsx

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,16 +1,23 @@
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import css from './Balance.module.css';
+import { selectFinance } from 'redux/transactions/transactionsSelectors';
+
 export const Balance = () => {
-  // const balance = useSelector(selectBalance);
-  
+  const balance = useSelector(selectFinance);
+
+  // const balance = useSelector(state => state.auth.user.balance);
+
+  const bal = balance[balance.length - 1];
+  console.log(balance);
+  console.log(bal);
 
   return (
     <div className={css.balanceWrapper}>
       <p className={css.balanceText}>Your balance</p>
-      <p className={css.balance}>
-        <span className={css.balanceSign}>&#8372;</span>
-        {/* {balance.toFixed(2)} */} 24 000.00
-      </p>
+      {/* <p className={css.balance}>
+          <span className={css.balanceSign}>&#8372; </span>
+          {balance.toFixed(2)}
+        </p> */}
     </div>
   );
 };
