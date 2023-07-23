57d757d905b8c587f177bda8b2ce2cfc410b43b8
Danylo Diachenko <danylodiachenko@gmail.com>
2023-07-23 14:50:38 +0300

    commit

--- src/components/HomeTab/HomeTab.jsx
+++ src/components/HomeTab/HomeTab.jsx
@@ -1,13 +1,11 @@
 import TransactionsList from 'components/TransactionsList/TransactionsList';
 import { useSelector } from 'react-redux';
-import { selectFinance } from 'redux/transactions/transactionsSelectors';
+import { selectFinanseSort } from 'redux/transactions/transactionsSelectors';
 // import css from './HomeTab.module.css';
 
 const HomeTab = () => {
-  const finance = useSelector(selectFinance);
-  const finanseSort = [...finance]
-    .reverse()
-    .sort((a, b) => new Date(b.transactionDate) - new Date(a.transactionDate));
+  const finanseSort = useSelector(selectFinanseSort);
+
   const Scrol = () => {
     if (finanseSort.length >= 7) {
       return true;
--- src/redux/transactions/transactionsSelectors.js
+++ src/redux/transactions/transactionsSelectors.js
@@ -1,3 +1,11 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectFinance = state => state.transactions.finance;
 export const selectCategories = state => state.transactions.categories;
 export const selectLoading = state => state.transactions.loading;
+
+export const selectFinanseSort = createSelector([selectFinance], items => {
+  return [...items]
+    .reverse()
+    .sort((a, b) => new Date(b.transactionDate) - new Date(a.transactionDate));
+});
