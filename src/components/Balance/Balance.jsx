1278e45378b70b50ae4612ad6e2a27202eb6b089
endorf8n <128092594+endorf8n@users.noreply.github.com>
2023-07-23 21:54:48 +0300

    add formatter, css style for modal

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,28 +1,22 @@
 import { useSelector } from 'react-redux';
 import css from './Balance.module.css';
-// import { selectFinance } from 'redux/transactions/transactionsSelectors';
 import { selectUser } from 'redux/auth/authSelectors';
 
 export const Balance = () => {
-  // const balance = useSelector(selectFinance);
   const user = useSelector(selectUser);
-
-  // const bal = balance[balance.length - 1];
+  const formatBallance = balance => {
+    const [integerPart, decimalPart] = balance.toFixed(2).split('.');
+    const formattedInteger = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+    return `${formattedInteger}.${decimalPart}`;
+  };
   const authUser = user?.balance;
-  // const userBalance = bal?.balanceAfter;
-  const number = 0;
 
   return (
     <div className={css.balanceWrapper}>
       <p className={css.balanceText}>Your balance</p>
       <p className={css.balance}>
         <span className={css.balanceSign}>&#8372; </span>
-
-        {authUser.length === 0 ? number.toFixed(2) : authUser?.toFixed(2)}
-        {/* 
-        {balance.length === 0
-          ? balanceUser?.toFixed(2)
-          : userBalance?.toFixed(2)} */}
+        {formatBallance(authUser)}
       </p>
     </div>
   );
